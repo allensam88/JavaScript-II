55a9b6b41d6e3cc273a9804a6becbd548c43504a
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-09-17 15:58:41 -0700

    finished MVP and some stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -69,7 +69,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 // console.log(sumNums(2, 5, add));
 // //End of Question 3
 
-// //Question 4
+//Question 4
 // function multiplyNums(x, y, cb) {
 //   return cb(x, y);
 //   // multiplyNums multiplies two numbers and passes the result to the callback.
@@ -80,17 +80,21 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 // console.log(multiplyNums(2, 5, multiply));
 // //End of Question 4
 
-// //Question 5
-// function contains(item, list, cb) {
-//   // contains checks if an item is present inside of the given array/list.
-//   // Pass true to the callback if it is, otherwise pass false.
-//   return cb(item, list);
-// }
-// //Callback Function
-// const check = (item, list) => if(list.includes(item)){`${item} is present`};
-// //Test
-// console.log(contains('Pencil', items, check));
-// //End of Question 5
+//Question 5
+function contains(item, list, cb) {
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list)
+}
+//Callback Function
+function check(item, list){
+  if(list.includes(item)){
+    return `${item} is present.`;
+  } else{return `${item} is NOT present.`;}
+}
+//Test
+console.log(contains('Pencil', items, check));
+//End of Question 5
 
 /***************BREAK****************/
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -3,39 +3,39 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-let nameFirst = "Sam";
-function parentFunction(){
-  let nameLast = "Allen";
-  function childFunction(){
-    console.log(`Hi my name is, ${nameFirst} ${nameLast}`);
-  }
-  childFunction();
-}
-parentFunction();
+// let nameFirst = "Sam";
+// function parentFunction(){
+//   let nameLast = "Allen";
+//   function childFunction(){
+//     console.log(`Hi my name is, ${nameFirst} ${nameLast}`);
+//   }
+//   childFunction();
+// }
+// parentFunction();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
-  let count = 0;
-  return function (){
-    return count = count + 1;
-  }
-};
-const newCounter = counterMaker();
-// console.log(newCounter);
+// const counterMaker = () => {
+//   // IMPLEMENTATION OF counterMaker:
+//   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+//   // 2- Declare a function `counter`. It should increment and return `count`.
+//   //      NOTE: This `counter` function, being nested inside `counterMaker`,
+//   //      "closes over" the `count` variable. It can "see" it in the parent scope!
+//   // 3- Return the `counter` function.
+//   let count = 0;
+//   return function (){
+//     return count = count + 1;
+//   }
+// };
+// const newCounter = counterMaker();
+// // console.log(newCounter);
 
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
+// console.log(newCounter());
+// console.log(newCounter());
+// console.log(newCounter());
+// console.log(newCounter());
 
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
